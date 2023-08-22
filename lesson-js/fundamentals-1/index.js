d6e0779b64f5988b48f3739e5f13b0086b4f23f2
kerbethecoder <krbycnts@gmail.com>
2023-08-22 21:57:53 +0800

    started lesson on js

---
+++ lesson-js/fundamentals-1/index.js
@@ -0,0 +1,9 @@
+console.log('Hello, my World!');
+
+let user = 'Tricia',
+  age = 24,
+  message = 'Hi Kerby!';
+
+console.log(
+  message + " I'm " + user + " and I'm " + age + ' years old! Douzoyouroshiku!'
+);
---
+++ lesson-js/fundamentals-1/tasks/index.js
@@ -0,0 +1,21 @@
+'use strict';
+// use this line above to avoid errors about variables
+// mean = 123;
+// console.log(mean);
+
+// task 1
+let admin, name;
+name = 'John';
+admin = name;
+console.log(admin);
+
+// task 2
+let planetName = 'Earth';
+let currentUser = 'John';
+
+// task 3
+function someCode(parameter) {}
+
+const BIRTHDAY = '18.04.1982';
+// uppercase because it is hard-coded
+const age = someCode(BIRTHDAY);
